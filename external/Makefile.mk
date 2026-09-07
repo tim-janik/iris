25cfe3a05f6b42ddaba9d32b739caab41ac735f8
Tim Janik <timj@gnu.org>
2026-09-07 23:25:55 +0000

    external: fetch mermaid.js 11.17.2

--- external/Makefile.mk
+++ external/Makefile.mk
@@ -13,7 +13,11 @@ highlightjs/css_sha := 554e678b27d0ddbcca9b262965c55fabbea13e902673d402a0b86384d
 highlightjs/js_url := https://cdnjs.cloudflare.com/ajax/libs/highlight.js/$(highlightjs/version)/highlight.min.js
 highlightjs/css_url := https://cdnjs.cloudflare.com/ajax/libs/highlight.js/$(highlightjs/version)/styles/github.min.css
 
-EXTERNAL_STAMPS := external/highlight.js/.sha-$(highlightjs/js_sha)-$(highlightjs/css_sha)
+mermaidjs/version := 11.17.2
+mermaidjs/js_sha := 581ed7d74bd9048d0e3a91363927d72ef22942d7722546b27f7cc29e35390eb8
+mermaidjs/js_url := https://cdn.jsdelivr.net/npm/mermaid@$(mermaidjs/version)/dist/mermaid.min.js
+
+EXTERNAL_STAMPS := external/highlight.js/.sha-$(highlightjs/js_sha)-$(highlightjs/css_sha) external/mermaid/.sha-$(mermaidjs/js_sha)
 
 .PHONY: all
 all: $(EXTERNAL_STAMPS)
@@ -22,6 +26,11 @@ external/highlight.js/.sha-$(highlightjs/js_sha)-$(highlightjs/css_sha):
 	mkdir -p external/highlight.js/styles
 	$(call fetch-and-check, external/highlight.js/highlight.min.js, $(highlightjs/js_sha), $(highlightjs/js_url))
 	$(call fetch-and-check, external/highlight.js/styles/github.min.css, $(highlightjs/css_sha), $(highlightjs/css_url))
+	touch $@
+
+external/mermaid/.sha-$(mermaidjs/js_sha):
+	mkdir -p external/mermaid
+	$(call fetch-and-check, external/mermaid/mermaid.min.js, $(mermaidjs/js_sha), $(mermaidjs/js_url))
 	touch $@
 
 $(EXTERNAL_STAMPS): external/Makefile.mk
